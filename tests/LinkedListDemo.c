7a553dfe9b21325c9d6c5074021e93aba0505856
douglasaubre51 <boseallen192@gmail.com>
2025-04-17 13:22:23 +0530

    1. added insertEnd!

--- tests/LinkedListDemo.c
+++ tests/LinkedListDemo.c
@@ -7,19 +7,19 @@
 // getters and setters
 // setter for value
 int setValue(){
-  printf("enter a no:");
-  int num;
-  scanf("%d",&num);
+    printf("enter a no:");
+    int num;
+    scanf("%d",&num);
 
-  return num;
+    return num;
 }
 // setter for key
 int setKey(){
-  printf("enter a key:");
-  int num;
-  scanf("%d",&num);
+    printf("enter a key:");
+    int num;
+    scanf("%d",&num);
 
-  return num;
+    return num;
 }
 
 // getter for value
@@ -42,8 +42,8 @@ void showValue(int num){ printf("%d\t",num); }
 
 // linked list structure
 LIST{
-  int data;
-  LIST* link;
+    int data;
+    LIST* link;
 };
 
 LIST* headPtr=NULL;
@@ -51,121 +51,147 @@ LIST* headPtr=NULL;
 
 // linked list methods
 LIST* createNode(int item){
-  LIST* ptr=(LIST*) malloc (sizeof(LIST));
-  ptr->data=item;
-  ptr->link=NULL;
+    LIST* ptr=(LIST*) malloc (sizeof(LIST));
+    ptr->data=item;
+    ptr->link=NULL;
 
-  return ptr;
+    return ptr;
 }
 
 bool isEmpty(){ return (headPtr==NULL) ? true : false; }
 
+// insert
 void insertFront(int item){
-  LIST* ptr=headPtr;
-
-  if(isEmpty()){
-    headPtr=createNode(item);
-    return;
-  }
-
-  else{
-    LIST* newPtr=createNode(item);
-    newPtr->link=ptr;
-    headPtr=newPtr;
-
-    return;
-  }
-}
-
-void traverse(){
-  if(isEmpty()){
-    listEmpty();
-    return;
-  }
-
-  else if(headPtr->link==NULL){
-    showValue(headPtr->data);
-    return;
-  }
-
-  else{
     LIST* ptr=headPtr;
 
-    while(ptr!=NULL){
-      if(ptr->link==NULL){
-	showValue(ptr->data);
+    if(isEmpty()){
+	headPtr=createNode(item);
+	return;
+    }
+
+    else{
+	LIST* newPtr=createNode(item);
+	newPtr->link=ptr;
+	headPtr=newPtr;
 
 	return;
-      }
-
-      else{
-	showValue(ptr->data);
-
-       	ptr=ptr->link;
-      }
     }
-  }
+}
+
+void insertEnd(int item){
+    if(isEmpty()){
+	headPtr=createNode(item);
+	return;
+    }
+
+    else{
+	LIST* ptr=headPtr;
+
+	while(ptr!=NULL){
+	    if(ptr->link==NULL){
+		LIST* newPtr=createNode(item);
+		ptr->link=newPtr;
+
+		return;
+	    }
+
+	    else{
+		ptr=ptr->link;
+	    }
+	}
+    }
+}
+
+// misc
+void traverse(){
+    if(isEmpty()){
+	listEmpty();
+	return;
+    }
+
+    else if(headPtr->link==NULL){
+	showValue(headPtr->data);
+	return;
+    }
+
+    else{
+	LIST* ptr=headPtr;
+
+	while(ptr!=NULL){
+	    if(ptr->link==NULL){
+		showValue(ptr->data);
+
+		return;
+	    }
+
+	    else{
+		showValue(ptr->data);
+
+		ptr=ptr->link;
+	    }
+	}
+    }
 }
 
 
 // linked list builder
 void main(){
-  printf("Linked List :-\n");
+    printf("Linked List :-\n");
 
-  printf("1.{Insert Front}\n");
-  printf("2.{Insert End}\n");
-  printf("3.{Insert After any Key}\n");
-  printf("4.{Delete Front}\n");
-  printf("5.{Delete End}\n");
-  printf("6.{Delete any Key}\n");
-  printf("7.{Traverse List}\n");
-  printf("8.{Find by Key}\n");
-  printf("0.{Exit Cli}\n");
+    printf("1.{Insert Front}\n");
+    printf("2.{Insert End}\n");
+    printf("3.{Insert After any Key}\n");
+    printf("4.{Delete Front}\n");
+    printf("5.{Delete End}\n");
+    printf("6.{Delete any Key}\n");
+    printf("7.{Traverse List}\n");
+    printf("8.{Find by Key}\n");
+    printf("0.{Exit Cli}\n");
 
-  for(;;){
-    printf("\n\nchoice:");
-    int choice;
-    scanf("%d",&choice);
+    for(;;){
+	printf("\n\nchoice:");
+	int choice;
+	scanf("%d",&choice);
 
-    int temp;
-    bool ok;
-    switch(choice){
-      default:printf("invalid choice!\n\a");
-	      break;
+	int temp;
+	bool ok;
+	switch(choice){
+	    default:printf("invalid choice!\n\a");
+		    break;
 
-      case 0:printf("bye!\n");
-	     return;
-	     break;
+	    case 0:printf("bye!\n");
+		   return;
+		   break;
 
-      case 1:insertFront(setValue());
-	     success();
-	     break;
+	    case 1:insertFront(setValue());
+		   success();
+		   break;
 
-//      case 2:insertEnd(setValue());
-//	     success();
-//	     break;
-//
-//      case 3:temp=insertAfterAnyKey(setKey(),setValue());
-//	     (temp==-1) ? keyNotFound() : success();
-//	     break;
-//
-//      case 4:temp=deleteFront();
-//	     (temp==-1) ? listEmpty() : getValue(temp);
-//
-//      case 5:temp=deleteEnd();
-//	     (temp==-1) ? listEmpty() : getValue(temp);
-//
-//      case 6:temp=deleteAnyKey(setKey());
-//	     (temp==-1) ? keyNotFound() : getValue(temp);
-//	     break;
+	    case 2:insertEnd(setValue());
+		   success();
+		   break;
+		   //
+		   //      case 3:temp=insertAfterAnyKey(setKey(),setValue());
+		   //	     (temp==-1) ? keyNotFound() : success();
+		   //	     break;
+		   //
+		   //      case 4:temp=deleteFront();
+		   //	     (temp==-1) ? listEmpty() : getValue(temp);
+		   //
+		   //      case 5:temp=deleteEnd();
+		   //	     (temp==-1) ? listEmpty() : getValue(temp);
+		   //
+		   //      case 6:temp=deleteAnyKey(setKey());
+		   //	     (temp==-1) ? keyNotFound() : getValue(temp);
+		   //	     break;
 
-      case 7:traverse();
-	     break;
+	    case 7:traverse();
+		   break;
 
-//      case 8:temp=findByKey(setKey());
-//	     (temp==-1) ? keyNotFound() : getValue(temp);
-//	     break;
+		   //      case 8:temp=findByKey(setKey());
+		   //	     (temp==-1) ? keyNotFound() : getValue(temp);
+		   //	     break;
+	}
     }
-  }
 }
 
